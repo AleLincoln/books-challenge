7513f0c096e466b20801c3aab8f2813cbd2b49ba
AleLincoln <50921123+AleLincoln@users.noreply.github.com>
2020-07-19 18:10:47 +0100

    little review

---
+++ script.js
@@ -0,0 +1,20 @@
+function loadBooks(myjson){
+    const ul = document.createElement('ul');
+
+    myjson.foEach((item) => {
+        let li = document.createElement('li')
+        let author = document.createElement('h4')
+        author.innerHTML = item.author
+        let title = document.createElement('h3')
+        title.innerHTML = item.title
+        let publisher = document.createElement('h2')
+        publisher.innerHTML = item.publisher
+
+        li.appendChild(author)
+        li.appendChild(title)
+        li.appendChild(publisher)
+        ul.appendChild(li)
+    })
+
+
+}
